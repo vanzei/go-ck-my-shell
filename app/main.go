bdfb9c40b0b37c9e4e67ef416f60637f6b83a32f
vanzei <leonardo.vanzei123@gmail.com>
2025-08-30 11:35:38 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,11 +10,10 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
 	// Wait for user input
 	for {
+
+		fmt.Fprint(os.Stdout, "$ ")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
