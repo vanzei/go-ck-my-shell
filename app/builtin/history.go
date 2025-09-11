efc0ae8b177bc9b1215cd4d3f45f7d155480e8c2
vanzei <leonardo.vanzei123@gmail.com>
2025-09-10 22:36:25 -0400

    codecrafters submit [skip ci]

--- app/builtin/history.go
+++ app/builtin/history.go
@@ -3,11 +3,26 @@ package builtin
 import (
 	"fmt"
 	"io"
+	"strconv"
 )
 
 func commandHistory(cfg *Config, w io.Writer) error {
-	for i, entry := range cfg.History {
-		fmt.Fprintf(w, "%d  %s\n", i+1, entry)
+	history := cfg.History
+	n := len(history) // default: show all
+
+	// If an argument is provided, parse it as a number
+	if len(cfg.CommandArgs) > 0 {
+		if num, err := strconv.Atoi(cfg.CommandArgs[0]); err == nil && num < n {
+			n = num
+		}
+	}
+
+	start := len(history) - n
+	if start < 0 {
+		start = 0
+	}
+	for i := start; i < len(history); i++ {
+		fmt.Fprintf(w, "%d  %s\n", i+1, history[i])
 	}
 	return nil
 }
